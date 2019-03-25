37ffb5ac676890766ae89fc979f8d04b983f22a2
Jørgen Vågan <jorgen@deepinsight.io>
2019-03-25 15:55:03 +0100

    deleted deleteme3.txt and changed hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
 def greet(name):
   print("Hi!", name +'!')
 
+def goodbye(name):
+  print('bye', name)
+
 if __name__ == '__main__':
-  greet("Jorgen")
+  name = 'Jorgen'
+  greet(name)
+  goodbye(name)
